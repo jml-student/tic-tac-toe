aac5c649532c817210eb924897c1f28722ebd95a
jml-student <student741@proton.me>
2024-06-05 14:42:49 +0200

    Finish styles and javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const status = document.querySelector(".status");
 let board = ["", "", "", "", "", "", "", "", ""];
-
+let gameActive = true;
+const cells = document.querySelectorAll(".cell");
 
 const Player = function (sign) {
     let score = 0;
@@ -12,8 +12,6 @@ const secondPlayer = Player("O");
 let currentPlayer = firstPlayer;
 
 const Game = (function () {
-    const status = document.querySelector(".status");
-
     const winningPatterns = [
         [0, 1, 2],
         [3, 4, 5],
@@ -38,14 +36,34 @@ const Game = (function () {
             let [a, b ,c] = winningPatterns[i];
             if (board[a] !== "" && board[a] === board[b] && board[b] === board[c]) {
                 status.textContent = `${currentPlayer.sign} is the winner!`;
+                cells[a].style.border = "3px solid green";
+                cells[a].style.boxShadow = "green 0px 0px 5px 3px";
+                cells[b].style.border = "3px solid green";
+                cells[b].style.boxShadow = "green 0px 0px 5px 3px";
+                cells[c].style.border = "3px solid green";
+                cells[c].style.boxShadow = "green 0px 0px 5px 3px";
                 currentPlayer.score++;
                 displayScores();
+                gameActive = false;
+                setTimeout(() => {
+                    newTurn();
+                    clearStyles();
+                }, 2000);
                 break;
             };
         }
     };
 
+    const clearStyles = () => {
+        for (let cell of cells) {
+            cell.style.border = "";
+            cell.style.boxShadow = "";
+        }
+    };
+
     const newTurn = () => {
+        gameActive = true;
+        const status = document.querySelector(".status");
         board = ["", "", "", "", "", "", "", "", ""];
         currentPlayer = firstPlayer;
         status.textContent = `${currentPlayer.sign} turn`;
@@ -53,25 +71,25 @@ const Game = (function () {
     }
 
     const resetGame = () => {
+        const status = document.querySelector(".status");
         board = ["", "", "", "", "", "", "", "", ""];
         firstPlayer.score = 0;
         secondPlayer.score = 0;
+        displayScores();
         currentPlayer = firstPlayer;
         status.textContent = `${currentPlayer.sign} turn`;
         document.querySelectorAll(".cell").forEach(cell => cell.textContent = "");
     }
 
-    return { checkWin, newTurn, resetGame};
+    return { checkWin, clearStyles, newTurn, resetGame};
 
 })();
-
-const cells = document.querySelectorAll(".cell");
 
 cells.forEach((cell, index) => {
     const status = document.querySelector(".status");
 
     cell.addEventListener("click", () => {
-        if (board[index] === "") {
+        if (gameActive && board[index] === "") {
                 cell.textContent = currentPlayer.sign;
                 if (currentPlayer.sign === "X") {
                     cell.setAttribute("style", "color: rgb(223, 145, 0);")
@@ -81,9 +99,19 @@ cells.forEach((cell, index) => {
                 board[index] = currentPlayer.sign;
                 Game.checkWin();
                 currentPlayer = currentPlayer === firstPlayer ? secondPlayer : firstPlayer;
+                if (gameActive) {
+                    status.textContent = `${currentPlayer.sign} turn`;
+                }
             };
         }
     );
+});
+
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", () => {
+    if (gameActive) {
+        Game.resetGame();
+    }
 });
 
 Game.resetGame();
